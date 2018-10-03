21b30fcde77752fd5333557447e6aba5dec0437e
Jonathan <j.olaleye.o@gmail.com>
2018-10-02 20:56:17 -0500

    Move comment

--- tasks/init.js
+++ tasks/init.js
@@ -1,5 +1,3 @@
-// initialize the vault (boilerplate save location)
-
 const fs = require('fs-extra');
 const os = require('os');
 const path = require('path');
@@ -7,6 +5,7 @@ const chalk = require('chalk');
 
 const vault = path.resolve(os.homedir(), '.spark');
 
+// initialize the vault (boilerplate save location)
 fs.ensureDir(vault)
   .then(() => console.log(chalk.green(`Spark vault successfully initialized @ ${chalk.cyan(vault)}!`)))
   .catch(err => console.error(err));
