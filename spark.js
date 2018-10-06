aa54b63131e493f3d635eb6aa45a3f10387e9442
Jonathan <j.olaleye.o@gmail.com>
2018-10-05 23:25:53 -0500

    Revise help info and chalk colors, minor code changes

--- spark.js
+++ spark.js
@@ -3,13 +3,13 @@
 const cli = require('commander');
 const chalk = require('chalk');
 
-const package = require('./package.json');
+const pkg = require('./package.json');
 const save = require('./tasks/save');
 
 // cli info
 cli
-  .version(package.version, '-v, --version')
-  .usage(`${chalk.green('<command>')} ${chalk.gray('or')} spark ${chalk.green('<boilerplate-name> <project-directory>')}`)
+  .version(pkg.version, '-v, --version')
+  .usage(`${chalk.yellow('<command>')} ${chalk.gray('or')} spark ${chalk.yellow('<boilerplate-name> <project-directory>')}`)
   .on('--help', () => {
     console.log('\nFor example, if you wanted to save and use a boilerplate for React projects...');
     console.log(`  spark save react https://github.com/user/react-starter.git`);
@@ -27,11 +27,11 @@ cli
 // save - save a directory or repository to a name
 cli
   .command('save <name> [source]')
-  .usage(`${chalk.green('<name>')} ${chalk.greenBright('[source]')}`)
+  .usage(chalk.yellow('<name> [source]'))
   .description('save a directory or respository')
   .on('--help', () => {
-    console.log(`\n${chalk.greenBright('[source]')} can be...`);
-    console.log(`  - a local path to a directory relative to the current directory: ${chalk.cyan('./starter')}`);
+    console.log(`\n${chalk.yellow('[source]')} can be...`);
+    console.log(`  - a path to a directory: ${chalk.cyan('./starter')}`);
     console.log(`  - a Git repository URL: ${chalk.cyan('https://github.com/user/starter.git')}`);
     console.log('If no source is provided, it will default to the current directory.\n');
   })
@@ -47,7 +47,7 @@ cli
 // show - display the file structure of a boilerplate
 cli
   .command('show <name>')
-  .usage(`${chalk.green('<name>')}`)
+  .usage(`${chalk.yellow('<name>')}`)
   .description('display the file structure of a boilerplate')
   .action((name) => {});
 
@@ -55,10 +55,10 @@ cli
 cli
   .command('delete <names...>')
   .alias('rm')
-  .usage(`${chalk.green('<names...>')}`)
+  .usage(`${chalk.yellow('<names...>')}`)
   .description('delete a saved boilerplate')
   .on('--help', () => {
-    console.log(`\n${chalk.green('<names...>')} can be one or multiple names of saved boilerplates.\n`);
+    console.log(`\n${chalk.yellow('<names...>')} can be one or multiple names of saved boilerplates.\n`);
   })
   .action((names) => {});
 
