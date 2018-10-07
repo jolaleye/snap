b199421c86c89ef9b3a038530dc142656b9de295
Jonathan <j.olaleye.o@gmail.com>
2018-10-07 00:28:10 -0500

    Check if name provided exists, improve structure logging

--- tasks/show.js
+++ tasks/show.js
@@ -9,19 +9,25 @@ const chalk = require('chalk');
 // display the file structure of a boilerplate
 function show(name) {
   const root = path.resolve(os.homedir(), '.spark', name);
+  const exists = fs.pathExistsSync(root);
+  if (!exists) {
+    console.log(`\n${chalk.redBright('Error:')} ${name} does not exist.\n`);
+    return;
+  }
+
   const tree = walk(root, 0);
   console.log();
   printTree(tree);
   console.log();
 }
 
-function printTree(item) {
+function printTree(item, isLast) {
   if (item.level === 0) console.log(chalk.yellow(item.name));
   else {
     // build the item's indent/prefix
     let prefix = '';
     prefix += '│    '.repeat(item.level - 1);
-    prefix += '├── ';
+    prefix += isLast ? '└── ' : '├── ';
 
     console.log(`${prefix}${item.name}`);
   }
@@ -29,7 +35,8 @@ function printTree(item) {
   // recursively print children
   if (item.children) {
     for (const child of item.children) {
-      printTree(child);
+      const isLastChild = child === item.children[item.children.length - 1];
+      printTree(child, isLastChild);
     }
   }
 }
@@ -37,9 +44,9 @@ function printTree(item) {
 function walk(currentPath, level) {
   // give the current item a name & level
   const item = { name: path.basename(currentPath), level };
-  // get it's stats
+  // get its stats
   const stats = fs.statSync(currentPath);
-  // check out the item's contents if it has any
+  // look at the item's contents if it has any
   if (stats.isDirectory()) {
     item.children = fs.readdirSync(currentPath).map(child => walk(path.join(currentPath, child), level + 1));
   }
